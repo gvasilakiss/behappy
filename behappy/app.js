b5a825a3023bb9fed0d32d5d2fbdf0d2f2a76db6
gvasilakis <34243942+gvasilakiss@users.noreply.github.com>
2021-02-17 18:54:08 +0000

    Update app.js

--- behappy/app.js
+++ behappy/app.js
@@ -23,10 +23,6 @@ app.get('/:text', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
 app.get('/*', (req, res) => {
   res.send("<h1>Put your message after the slash (/) to display your current mood!</h1>")
 });
